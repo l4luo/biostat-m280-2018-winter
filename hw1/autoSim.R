6f465765cd1a693b6e879d4096d09878c593a307
Landi Luo <l4luo@ucla.edu>
2018-02-02 07:49:41 +0000

    Update autoSim.R

--- hw1/autoSim.R
+++ hw1/autoSim.R
@@ -3,18 +3,14 @@ nVals <- seq(100, 500, by=100)
 distVals <- c("gaussian", "t1", "t5")
 seed <- 280
 rep <- 50
-#distVals = rep(c("gaussian","t1","t5"), 5)
 
 for (n in nVals) {
   for (dist in distVals) {
     oFile <- paste(dist, "_n", n, ".txt", sep="")
-    arg <- paste(paste("seed=", seed, sep=""), paste("n=", n, sep=""), 
-                 paste("'dist=\"", dist, "\"'", sep=""), paste("rep=", rep, sep=""))
+    arg <- paste(paste("seed=", seed, " n=", n, " 'dist=\"", dist, "\"'", 
+                       " rep=", rep, sep=""))
     sysCall <- paste("nohup Rscript runSim.R ", arg, " > ", oFile)
     system(sysCall)
     print(paste("sysCall=", sysCall, sep=""))
   }
 }
-
-
-
